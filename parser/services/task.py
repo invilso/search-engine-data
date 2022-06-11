6ea1df46731d7bb9ad77bc2138cf6e01f23d6d13
invilso <40578604+invilso@users.noreply.github.com>
2022-06-11 22:20:41 +0300

    test task

--- parser/services/task.py
+++ parser/services/task.py
@@ -8,7 +8,7 @@ def main():
     # data = []
     # for v in search('Automotive workshops', num_results=100):
     #     data.append(v)
-    r = session.get(url='https://invilso.pp.ua/')
+    r = session.get(url='https://www.google.ru/maps/search/car+service/@38.1279033,-96.7198766,4z/data=!3m1!4b1')
     r.html.render()
     with open('file.html', encoding='utf-8', mode='w+') as f:
         f.write(r.text)
