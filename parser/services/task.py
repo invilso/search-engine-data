eebe0fcc7ecb9306cc08f0bb4a676905fbd97da5
invilso <40578604+invilso@users.noreply.github.com>
2022-06-11 15:26:14 +0300

    modify task

--- parser/services/task.py
+++ parser/services/task.py
@@ -1,7 +1,13 @@
+from requests import request, session
 from .google_search import search
+import requests_html
+
+session = requests_html.HTMLSession
 
 def main():
-    data = []
-    for v in search('Automotive workshops', num_results=100):
-        data.append(v)
-    return data
+    # data = []
+    # for v in search('Automotive workshops', num_results=100):
+    #     data.append(v)
+    r = session.get('gta-trinity.ru')
+    r.html.render()
+    return r.text
