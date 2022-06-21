2749eb3397411ea563fcc8cc36a6539e2913a311
invilso <40578604+invilso@users.noreply.github.com>
2022-06-21 13:54:06 +0300

    update process

--- parser/services/view_process.py
+++ parser/services/view_process.py
@@ -1,5 +1,14 @@
 from ..models import QueryesPool
+import bs4 
+import re
+
+def modify_html(html: str):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    input = soup.find('input')
+    text = input.get('value')
+    html2 = f'Left {QueryesPool.objects.all().count()} queryes'
+    return re.sub(text, f'{text} [[ {html2} ]]', html)
 
 def get_html():
-    html = f'<h1>Left {QueryesPool.objects.all().count()} queryes</h1>'
-    return html
+    with open('file.html', 'r', encoding='utf-8') as f:
+        return modify_html(html=f.read())
