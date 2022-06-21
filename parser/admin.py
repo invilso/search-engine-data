9166960999407b7374df4b14042b4d874ebc182a
invilso <40578604+invilso@users.noreply.github.com>
2022-06-21 17:16:06 +0300

    .

--- parser/admin.py
+++ parser/admin.py
@@ -1,6 +1,7 @@
 import site
 from django.contrib import admin
-from .models import Site
+from .models import Site, QueryesPool
 # Register your models here.
 
 admin.site.register(Site)
+admin.site.register(QueryesPool)
